8704bb39343e1c67f8039d7c14ec170ba3d22974
Ahmad Alawneh <aalawneh19@gmail.com>
2023-11-05 00:08:46 +0300

    remove unneeded globals

--- mochaConfig.js
+++ mochaConfig.js
@@ -2,7 +2,5 @@ const chai = require('chai');
 
 global.sinon = require('sinon');
 global.expect = chai.expect;
-global.assert = chai.assert;
-global.should = chai.should;
 
 chai.use(require('sinon-chai'));
