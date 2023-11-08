523d54046445696387f902f353c1af51a451bad9
Ahmad Alawneh <aalawneh19@gmail.com>
2023-11-09 00:38:49 +0300

    Request wrappers + more eslint rules

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const requestService = require('./requestService');
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.use(requestService.logRequest);
+
+app.get('/', async (req, res, next) => {
+	return await requestService.handleRequest(req, res, next, async () => {
+		return res.send('Hello World!');
+	});
 });
+
+app.use(requestService.handleGenericError);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
